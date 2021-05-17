dde9b6c19347a2f02d25dcc72847e460569cbf9e
Beryl Armand <beryl.armand@gmail.com>
2021-05-17 12:23:04 +0200

    activeadmin for teachers

---
+++ app/admin/teachers.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register Teacher do
+  menu priority: 4
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :first_name, :last_name, :bio, :phone, :school_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:first_name, :last_name, :bio, :phone, :school_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  
+  
+end
