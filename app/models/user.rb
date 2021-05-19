5ba36025af45d5ffb344790d49f880887bb698d7
seryl lounis <seryllounis@outlook.fr>
2021-05-19 09:16:49 +0200

    maybe finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  validates :email, presence: true
-  validates :password, presence: true
   has_many :schools
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
