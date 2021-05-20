ebbcfa8163111c4a3347339d14eff46508068ecd
Beryl Armand <beryl.armand@gmail.com>
2021-05-20 13:01:57 +0200

    navbar responsive ok + addresses index front-fixes

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <Router>
-        <div className="h-screen flex overflow-hidden bg-white">
+        <div className="h-screen lg:flex overflow-auto bg-white">
           <Navbar />
           <Switch>
             <Route exact path="/school" component={School} />
