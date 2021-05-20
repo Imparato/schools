66fb73d638584915195aed67179a171133172f06
damien vincent <vincent.damien@gmail.com>
2021-05-20 10:38:11 +0200

    Copy tags init in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,28 @@ Network.destroy_all
 Property.destroy_all
 Teaching.destroy_all
 Teacher.destroy_all
+Property.destroy_all
 Course.destroy_all
 Address.destroy_all
 School.destroy_all
 User.destroy_all
+Tag.destroy_all
+
+
 
 # ------------------------------------------------- PREMIER TOUR -----------------------------------------------------
+
+puts "init tags"
+tags = {
+  jours: ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
+  niveaux: ["tous", "débutants", "confirmés"],
+  publics: ["tous", "à partir de 18 ans", "enfants", "adolescents", "adultes"],
+  categories: ["improvisation", "théâtre de texte", "mime"],
+  langues: ["en anglais", "en allemand", "en espagnol"]
+}
+tags.each do |category, names|
+  names.each { |name| Tag.create(name: name, category: category.to_s) }
+end
 
 puts "Starting round 1..."
 
