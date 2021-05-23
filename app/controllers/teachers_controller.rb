420503d5d296dc2e37f2e48246858a67c5dbd190
seryl lounis <seryllounis@outlook.fr>
2021-05-23 20:29:20 +0200

    fix bug heroku

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 class TeachersController < ApplicationController
   def index
     # @school = current_user.schools.find(params[:school_id])
-    teachers = School.find(params[:id].teachers
+    teachers = School.find(params[:id].teachers)
     render json: teachers
   end
 end
