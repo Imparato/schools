8f1c4891b3b0c98c1ebae4a6fbd66526d57d3c3d
Colin Darie <colin@darie.eu>
2021-06-09 10:35:23 +0200

    Test: Teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -7,8 +7,8 @@ class Teacher < ApplicationRecord
   validates_presence_of :first_name, message: "Veuillez préciser le nom de votre profésseur"
   validates :email, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "L'email n'est pas valide"}
   validates :phone, format: { with: /\A(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}/, message: "Le numero de téléphone ne semble pas correct" }
-  
+
   def full_name
-    return "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}"
   end
 end
--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class TeacherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should compute a full name" do
+    teacher = Teacher.new(first_name: "Toto", last_name: "Abitbol")
+    assert_equal "Toto Abitbol", teacher.full_name
+  end
+
+  test "should validates a phone number" do
+    teacher = Teacher.new(phone: "12345")
+    assert_not teacher.valid?
+    assert teacher.errors.key?("phone")
+
+    teacher = Teacher.new(phone: "0612345678")
+    teacher.valid?
+    assert_not teacher.errors.key?("phone")
+
+    teacher = Teacher.new(phone: "+33612345678")
+    teacher.valid?
+    assert_not teacher.errors.key?("phone")
+
+    teacher = Teacher.new(phone: "+336")
+    teacher.valid?
+    assert teacher.errors.key?("phone")
+
+    teacher = Teacher.new(phone: "+33 6 12 34 56 78")
+    teacher.valid?
+    assert_not teacher.errors.key?("phone")
+  end
 end
