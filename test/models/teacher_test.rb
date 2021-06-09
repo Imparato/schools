dc1db2ba2255e69a62f735cbf40efeb376b32aec
Colin Darie <colin@darie.eu>
2021-06-09 11:16:56 +0200

    Test with factory_bot

--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -16,29 +16,26 @@ require "test_helper"
 
 class TeacherTest < ActiveSupport::TestCase
   test "should compute a full name" do
-    teacher = Teacher.new(first_name: "Toto", last_name: "Abitbol")
+    teacher = build(:teacher, first_name: "Toto", last_name: "Abitbol")
     assert_equal "Toto Abitbol", teacher.full_name
   end
 
   test "should validates a phone number" do
-    teacher = Teacher.new(phone: "12345")
+    assert build(:teacher).valid?
+
+    teacher = build(:teacher, phone: "12345")
     assert_not teacher.valid?
-    assert teacher.errors.key?("phone")
 
-    teacher = Teacher.new(phone: "0612345678")
-    teacher.valid?
-    assert_not teacher.errors.key?("phone")
+    teacher = build(:teacher, phone: "0612345678")
+    assert teacher.valid?
 
-    teacher = Teacher.new(phone: "+33612345678")
-    teacher.valid?
-    assert_not teacher.errors.key?("phone")
+    teacher = build(:teacher, phone: "+33612345678")
+    assert teacher.valid?
 
-    teacher = Teacher.new(phone: "+336")
-    teacher.valid?
-    assert teacher.errors.key?("phone")
+    teacher = build(:teacher, phone: "+336")
+    assert_not teacher.valid?
 
-    teacher = Teacher.new(phone: "+33 6 12 34 56 78")
-    teacher.valid?
-    assert_not teacher.errors.key?("phone")
+    teacher = build(:teacher, phone: "+33 6 12 34 56 78")
+    assert teacher.valid?
   end
 end
