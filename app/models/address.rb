7a0aa0db8830eb9bfa71f028f9130a289e01d78d
damien vincent <vincent.damien@gmail.com>
2021-06-17 12:10:54 +0200

    Enhance address zipcode

--- app/models/address.rb
+++ app/models/address.rb
@@ -30,10 +30,10 @@ class Address < ApplicationRecord
   validates_presence_of :address, message: "Veuillez entrer une addresse"
   validates_presence_of :city, message: "Veuillez entrer une ville"
   # validates :address, presence: true, message: "Veuillez entrer une addresse"
-  # validates :city, presence: true, with_message: 'Veuillez entrer une ville' 
+  # validates :city, presence: true, with_message: 'Veuillez entrer une ville'
   # validates_format_of :phone, with: /\A(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}/, message: "Le numero de téléphone ne semble pas correct"
-  validates :zipcode, format: {with: /\A(?:[0-8]\d|9[0-8])\d{3}/, message: "Veuillez entrer un code postal valide"}
-  
+  validates :zipcode, format: {with: /\A(((?:[0-8]\d|9[0-8])\d{3})|(QC )?\w\d\w \d\w\d|(CH-)?\d{4})/, message: "Veuillez entrer un code postal valide"}
+
   def to_s
     return address
   end
